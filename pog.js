3545945cffa88971556dae1d542bb861e82c49ee
ElBedwihy <58118886+YassinElBedwihy@users.noreply.github.com>
2021-01-24 11:12:01 +0200

    Add files via upload
    finished the delete button, took some time because i had to think of some sort of system to manage the row numbers. thank god.

--- pog.js
+++ pog.js
@@ -3,9 +3,9 @@ let date = document.getElementById('date'); //
 let nam = document.getElementById('name'); //   these are all normal inputs
 let amount = document.getElementById('amount'); //
 
-
-
 let table = document.getElementById('tabli')
+
+
 
 function getVal(){
     let newdate = date.value;
@@ -19,23 +19,55 @@ function getVal(){
     return [newdate, newname, newamount]
 }
 
-function updateTable(arr){
+function onclic(x){
+        table.deleteRow(x);
+}
+
+function updateTable([d, n, a]){
+
+   
+
     let row = table.insertRow(1);
     let celld = row.insertCell(0);
     let celln = row.insertCell(1);
     let cella = row.insertCell(2);
     let cellb = row.insertCell(3);
-    celld.innerHTML = arr[0];
-    celln.innerHTML = arr[1];
-    cella.innerHTML = arr[2];
+    celld.innerHTML = d;
+    celln.innerHTML = n;
+    cella.innerHTML = a;
+
+    
+    let button = document.createElement("input")
+
+    button.type = "button";
+    button.value = "x";
+    button.id = 1;
+    button.className = "deletebuttons"
+
+    cellb.append(button);
+    button.addEventListener("click", () => {
+        let id = button.id;
+        onclic(button.id)
+
+        Array.from(document.getElementsByClassName('deletebuttons')).forEach(button => {
+
+          if(button.id > id){
+          button.id--
+     
+        }}) 
+    })
 }
 
 
 let input = document.getElementById('inputbutton');
 
 input.addEventListener("click", () => {
+
+    Array.from(document.getElementsByClassName('deletebuttons')).forEach(button => button.id++)
+
     let arr = getVal();
     if(getVal() === null){
+        alert('invalid info input')
         return;
     }else {
         updateTable(arr);
